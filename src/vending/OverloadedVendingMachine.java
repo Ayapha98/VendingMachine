f4a29c91f1d976fed0db28dd1255f01998c9be8d
Ayapha98 <ayaphamihlali@gmail.com>
2020-05-19 11:42:26 +0200

    First commit

---
+++ src/vending/OverloadedVendingMachine.java
@@ -0,0 +1,33 @@
+package vending;
+
+public class OverloadedVendingMachine {
+
+    private int softDrinkQty;
+    private int saltySnackQty;
+    private int chocolateQty;
+
+   public OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty)
+    {
+
+    }
+
+
+    void buy (SoftDrink softDrink)
+    {
+
+    }
+    void buy (SaltySnack saltySnack)
+    {
+
+    }
+    void buy (Chocolate chocolate)
+    {
+
+    }
+    void buy (Product product)
+    {
+
+    }
+
+
+}
