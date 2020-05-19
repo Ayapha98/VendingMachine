9c1c246a03a2de57bd011c4f6c87ce40ec7c5aa3
Ayapha98 <ayaphamihlali@gmail.com>
2020-05-19 17:27:44 +0200

    Latest code

--- src/vending/OverloadedVendingMachine.java
+++ src/vending/OverloadedVendingMachine.java
@@ -7,9 +7,9 @@ import vending.product.SoftDrink;
 
 public class OverloadedVendingMachine {
 
-    private int softDrinkQty;
-    private int saltySnackQty;
-    private int chocolateQty;
+    private int softDrinkQty = 0;
+    private int saltySnackQty = 0;
+    private int chocolateQty = 0;
 
    public OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty)
     {
@@ -22,20 +22,23 @@ public class OverloadedVendingMachine {
 
     void buy (SoftDrink softDrink)
     {
-
+        System.out.println("Buying a soft drink");
+        softDrinkQty++;
     }
     void buy (SaltySnack saltySnack)
     {
-
+        System.out.println("Buying a salty snack");
+        saltySnackQty++;
     }
     void buy (Chocolate chocolate)
     {
-
+        System.out.println("Buying a chocolate");
+        chocolateQty++;
     }
     void buy (Product product)
     {
+        System.out.println("Buying a product");
 
     }
-
 
 }
